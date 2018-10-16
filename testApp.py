1f2f06088dafe8ad3056a2c4cf004d59a2a5a1a5
Utpal Kumar <utpalkumar50@gmail.com>
2018-10-17 02:22:13 +0800

    added random array and marker for plot

--- testApp.py
+++ testApp.py
@@ -2,10 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 array=np.linspace(1,100,10)
+randarray=np.random.rand(len(array))*10
 print(array)
 
 plt.figure(figsize=(10,6))
-plt.plot(array, color='r',linewidth=2)
+plt.plot(array,randarray,marker='o', color='r',linewidth=2)
 plt.xlabel("Data counts")
 plt.ylabel("Data values")
 plt.savefig('testFigure.png',dpi=100,bbox_inches='tight')
