9327ff84cf332068ac3a82b39197504ebb192ccf
Utpal Kumar <utpalkumar50@gmail.com>
2018-10-17 02:01:56 +0800

    changed the line color to red and increase the line width

--- testApp.py
+++ testApp.py
@@ -5,7 +5,7 @@ array=np.linspace(1,100,10)
 print(array)
 
 plt.figure(figsize=(10,6))
-plt.plot(array)
+plt.plot(array, color='r',linewidth=2)
 plt.xlabel("Data counts")
 plt.ylabel("Data values")
 plt.savefig('testFigure.png',dpi=100,bbox_inches='tight')
